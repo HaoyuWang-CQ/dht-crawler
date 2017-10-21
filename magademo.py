332f4e49cdcde3d95034c68bff3d2209de3328ba
HaoyuWang-CQ <wanghaoyu_hk@aliyun.com>
2017-10-21 21:48:05 +0800

    maga demo test

--- magademo.py
+++ magademo.py
@@ -1,23 +1,15 @@
-import asyncio
+from maga import Maga
 
-#
-async def wget(host):
-    print('wget %s...' % host)
-    connect = asyncio.open_connection(host, 80)
-    reader, writer = await connect
-    header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
-    writer.write(header.encode('utf-8'))
-    await writer.drain()
-    while True:
-        line = await reader.readline()
-        if line == b'\r\n':
-            break
-        print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
-    # Ignore the body, close the socket
-    writer.close()
+import logging
+logging.basicConfig(level=logging.INFO)
 
-loop = asyncio.get_event_loop()
-tasks = [wget(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
-loop.run_until_complete(asyncio.wait(tasks))
-loop.close()
 
+class Crawler(Maga):
+    async def handler(self, infohash, addr):
+        logging.info(addr)
+        logging.info(infohash)
+
+
+crawler = Crawler()
+# Set port to 0 will use a random available port
+crawler.run(port=0)
